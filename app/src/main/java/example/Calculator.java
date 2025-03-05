b942828e9cfac110c965ae834f963bdf2e871e52
amohdfarris <arifffikribin.mohdfarris@student.unimelb.edu.au>
2025-03-05 14:27:35 +1100

    Finished Calculator

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,25 +3,21 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        int c = a + b;
-        return c;
+        return a + b;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        int c = a - b;
-        return c;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
         // TODO: Implement this method
-        int c = a * b;
-        return c;
+        return a * b;
     }
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        int c = a / b;
-        return c;
+        return a / b;
     }
 }
